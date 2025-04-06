321f2d628e7444fd6fa3cfa7679ee5c6265d2e28
yusuf22123 <125798286+yusuf22123@users.noreply.github.com>
2025-04-06 19:43:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -276,4 +276,6 @@ def chat():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.getenv("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
